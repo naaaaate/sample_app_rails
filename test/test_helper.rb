1eb99486271e39eb0019c0015d724caceeb876f5
Nathan Lee <nlee43@gmail.com>
2015-09-23 11:23:27 -0700

    add backtrace silencer

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+# add minitest reporters and use it to add green and red to our tests
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
